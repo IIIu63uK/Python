a130bf7443ee5fc6f5fa745989074756d2808bf4
IIIu63uK <devseev92@gmail.com>
2024-02-03 21:48:15 -0400

    03.03.24

--- users.py
+++ users.py
@@ -7,4 +7,4 @@ for user in new_users:
     if user in current_users:
         print(f"{user} is not available, need choose other name.")
     else:
-        print(f"Name {user} is available.")
+        print(f"Name {user} is available.")11
